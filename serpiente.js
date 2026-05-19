f05571d27ac2e4742ccc90838b866d512901eab0
VinicioAyalaC <vhac2311@gmail.com>
2026-05-18 22:26:37 -0500

    construccion tablero

--- serpiente.js
+++ serpiente.js
@@ -3,8 +3,60 @@
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
 
+//*** PASO 4: CONSTANTE
+const TAMANIO_CELDA = 25;
 
-    
+
+//*** PASO 5: CREAR FUNCION dibujarTablero()
+
+function dibujarTablero(){
+  ctx.strokeStyle = "#333333";  // lineas color gris oscuro
+
+  ctx.beginPath();                // iniciar trazo, dibujar nueva linea
+  ctx.moveTo(25, 0);              // posicion inicial
+  ctx.lineTo(25, canvas.height);  // dibuja una linea desde y hasta
+  ctx.stroke();                   // dibuja la linea
+
+  /* // PASO 8: VARIAS LINEAS DE PRUEBA
+  ctx.beginPath();
+  ctx.moveTo(50, 0);
+  ctx.lineTo(50, canvas.height);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(75, 0);
+  ctx.lineTo(75, canvas.height);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(100, 0);
+  ctx.lineTo(100, canvas.height);
+  ctx.stroke();
+  fin de pruba paso 8   */
+
+
+  // PASO 9: CREAR FOR PARA DIBUJAR LAS LINEAS VERTICALES
+  for(let lineasX = TAMANIO_CELDA; lineasX < canvas.width; lineasX += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(lineasX, 0);
+    ctx.lineTo(lineasX, canvas.height);
+    ctx.stroke();
+  }
+
+  // PASO 10: CREAR FOR PARA DIBUJAR LAS LINEAS HORIZONTALES
+  for(let lineasY=TAMANIO_CELDA; lineasY < canvas.height; lineasY += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(0, lineasY);
+    ctx.lineTo(canvas.width, lineasY);
+    ctx.stroke();
+  }
+
+}//fin de dibujarTablero
+
+
+
+
+//****************ORIGINAL*****************/    
 
     // Primera pintura del juego al cargar la página
     dibujarTodo();
@@ -19,6 +71,7 @@
 
     function dibujarTodo() {
       limpiarCanvas();
+      dibujarTablero();   //PASO 7: INVOCAR dibujarTablero()
     }
 
 
